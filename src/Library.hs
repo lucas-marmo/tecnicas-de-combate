aecc7d4a4a443bb1f70e43b229f8115570d3e475
Lucas Marmo <lmarmo@frba.utn.edu.ar>
2021-04-27 14:34:10 -0300

    Se realizó la parte 3 del ejercicio

--- src/Library.hs
+++ src/Library.hs
@@ -1,5 +1,38 @@
 module Library where
 import PdePreludat
 
-doble :: Number -> Number
-doble numero = numero + numero
+type Horas = Number
+type Objetivo = String
+type Presion = Number
+
+presionGolpe :: Horas -> Objetivo -> Presion
+presionGolpe horas objetivo = poder horas / fortaleza objetivo
+
+poder :: Horas -> Number
+poder = (* 15)
+
+fortaleza :: Objetivo -> Number
+fortaleza = (2*) . length
+
+type Golpe = Objetivo -> Number
+
+gomuGomu :: Golpe
+gomuGomu = presionGolpe 180
+
+normalesConsecutivos :: Golpe 
+normalesConsecutivos = presionGolpe 240
+
+objetivoEsDificil :: Objetivo -> Bool
+objetivoEsDificil objetivo = gomuGomu objetivo < 100
+
+objetivoEsAccesible :: Objetivo -> Bool
+objetivoEsAccesible = entre200y400.normalesConsecutivos.objetivoFocalizado
+
+entre200y400 :: Presion -> Bool
+entre200y400 presion = 200<presion && presion<400
+
+objetivoFocalizado :: Objetivo -> Objetivo
+objetivoFocalizado objetivo = primeras7Letras objetivo
+
+primeras7Letras :: Objetivo -> Objetivo
+primeras7Letras objetivo = take 7 objetivo
--- src/Spec.hs
+++ src/Spec.hs
@@ -6,6 +6,12 @@ import Test.Hspec
 correrTests :: IO ()
 correrTests = hspec $ do
   describe "Test de ejemplo" $ do
-    it "El pdepreludat se instaló correctamente" $ do
-      doble 1 `shouldBe` 2
+    it "Objetivo que recibe menos de 100 de presión con GomuGomu es difícil" $ do
+      objetivoEsDificil "bolsa de entrenamiento" `shouldBe` True
+    it "Objetivo que recibe más de 100 de presión con GomuGomu no es difícil" $ do
+      objetivoEsDificil "puf" `shouldBe` False
+    it "Objetivo focalizado que recibe entre 200 y 400 de presión con normalesConsecutivos es accesible" $ do
+      objetivoEsDificil "bolsa de entrenamiento" `shouldBe` True
+    it "Objetivo focalizado que recibe más de 400 de presión con normalesConsecutivos no es accesible" $ do
+      objetivoEsDificil "puf" `shouldBe` False
 
